51dc24bc4cf1c6946b023492d1b4f493b72780d0
Litlife <litlife.club@gmail.com>
2020-09-25 15:13:02 +0300

    fix

--- resources/views/blog/childs.blade.php
+++ resources/views/blog/childs.blade.php
@@ -1,6 +1,6 @@
 @if (!empty($descendants))
 	@foreach ($descendants as $descendant)
-		@if ($descendant->isDescendantOf($item))
+		@if (preg_match('/\,'.$item->id.'\,$/i', $descendant->tree))
 			@include('blog.list.default', ['item' => $descendant, 'parent' => $item])
 		@endif
 	@endforeach
--- resources/views/comment/descendants.blade.php
+++ resources/views/comment/descendants.blade.php
@@ -1,6 +1,6 @@
 @if (!empty($descendants))
 	@foreach ($descendants as $descendant)
-		@if ($descendant->isDescendantOf($comment))
+		@if (preg_match('/\,'.$comment->id.'\,$/i', $descendant->tree))
 			@include('comment.list.default', ['item' => $descendant, 'parent' => $comment, 'no_book_link' => true])
 		@endif
 	@endforeach
