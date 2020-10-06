7fdf027975f5a131a69580724beaffa7ad0ef073
Litlife <litlife.club@gmail.com>
2020-10-06 11:32:53 +0300

    favicon

---
+++ resources/views/layouts/favicons.blade.php
@@ -0,0 +1,11 @@
+<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png?v=oLnym07xey">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png?v=oLnym07xey">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png?v=oLnym07xey">
+<link rel="icon" type="image/x-icon" href="/favicons/favicon.ico?v=oLnym07xey"/>
+<link rel="manifest" href="/favicons/site.webmanifest?v=oLnym07xey">
+<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg?v=oLnym07xey" color="#5bbad5">
+<link rel="shortcut icon" href="/favicons/favicon.ico?v=oLnym07xey">
+<meta name="apple-mobile-web-app-title" content="LitLife">
+<meta name="application-name" content="LitLife">
+<meta name="msapplication-TileColor" content="#2b5797">
+<meta name="msapplication-config" content="/favicons/browserconfig.xml?v=oLnym07xey">
