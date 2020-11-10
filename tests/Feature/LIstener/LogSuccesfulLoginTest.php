c17f5463386cdaef47c2eb4ced3a766843a3e291
Litlife <litlife.club@gmail.com>
2020-11-10 14:23:59 +0300

    not to re-record the logs of user logins

--- tests/Feature/LIstener/LogSuccesfulLoginTest.php
+++ tests/Feature/LIstener/LogSuccesfulLoginTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Listener;
 
 use App\User;
+use App\UserAuthLog;
 use Illuminate\Auth\Events\Login;
 use Tests\TestCase;
 
@@ -33,5 +34,41 @@ class LogSuccesfulLoginTest extends TestCase
 		$this->assertTrue($this->app['session']->get('show_greeting'));
 
 		$this->assertNotNull($user->last_activity_at);
+	}
+
+	public function testDontWriteLogIfLatestSameUserAndTheTimeHasntPassedYet()
+	{
+		$user = factory(User::class)
+			->create();
+
+		$log = new UserAuthLog();
+		$log->user_id = $user->id;
+		$log->ip = request()->ip();
+		$log->save();
+
+		$this->assertEquals(1, $user->auth_logs()->count());
+
+		event(new Login('web', $user, true));
+
+		$this->assertEquals(1, $user->auth_logs()->count());
+	}
+
+	public function testWriteLogIfLatestSameUserAndTimePassed()
+	{
+		$user = factory(User::class)
+			->create();
+
+		$log = new UserAuthLog();
+		$log->user_id = $user->id;
+		$log->ip = request()->ip();
+		$log->save();
+
+		$this->assertEquals(1, $user->auth_logs()->count());
+
+		$this->travel(20)->seconds();
+
+		event(new Login('web', $user, true));
+
+		$this->assertEquals(2, $user->auth_logs()->count());
 	}
 }
